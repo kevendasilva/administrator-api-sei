695adeb3fb2d612308fa61e7fe80034083c826d3
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2023-06-14 04:04:13 -0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_024935) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_055158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,34 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_024935) do
     t.index ["email"], name: "index_administrators_on_email", unique: true
     t.index ["jti"], name: "index_administrators_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_administrators_on_reset_password_token", unique: true
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "last_name"
+    t.string "telephone"
+    t.string "cpf"
+    t.string "jti", null: false
+    t.index ["email"], name: "index_clients_on_email", unique: true
+    t.index ["jti"], name: "index_clients_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
+  end
+
+  create_table "movements", force: :cascade do |t|
+    t.float "cost"
+    t.bigint "vehicle_id", null: false
+    t.bigint "client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_movements_on_client_id"
+    t.index ["vehicle_id"], name: "index_movements_on_vehicle_id"
   end
 
   create_table "parkings", force: :cascade do |t|
@@ -49,6 +77,29 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_024935) do
     t.index ["parking_id"], name: "index_vacancies_on_parking_id"
   end
 
+  create_table "vacancy_reservations", force: :cascade do |t|
+    t.bigint "client_id", null: false
+    t.bigint "vacancy_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_vacancy_reservations_on_client_id"
+    t.index ["vacancy_id"], name: "index_vacancy_reservations_on_vacancy_id"
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "nickname"
+    t.string "plate"
+    t.bigint "client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_vehicles_on_client_id"
+  end
+
+  add_foreign_key "movements", "clients"
+  add_foreign_key "movements", "vehicles"
   add_foreign_key "parkings", "administrators"
   add_foreign_key "vacancies", "parkings"
+  add_foreign_key "vacancy_reservations", "clients"
+  add_foreign_key "vacancy_reservations", "vacancies"
+  add_foreign_key "vehicles", "clients"
 end
