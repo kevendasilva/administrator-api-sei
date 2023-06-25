b74b2eee76377682ece81a8b55c2fa5b84d61a7e
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-06-25 12:05:11 -0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_25_140327) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_25_145445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_25_140327) do
     t.bigint "client_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "entry"
+    t.datetime "exit"
     t.index ["client_id"], name: "index_movements_on_client_id"
     t.index ["vehicle_id"], name: "index_movements_on_vehicle_id"
   end
