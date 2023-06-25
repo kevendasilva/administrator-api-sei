dfb50d2f22c3853c3604ebcc53dc0c3a75938c7e
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-06-25 09:36:24 -0300

    fix: sessions controller

--- app/controllers/administrators/sessions_controller.rb
+++ app/controllers/administrators/sessions_controller.rb
@@ -1,26 +1,24 @@
 # frozen_string_literal: true
 
 class Administrators::SessionsController < Devise::SessionsController
-  respond_to :json
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
 
-  private
-
-  def respond_with(resource, _opts = {})
     render json: {
-      message: 'Logged in sucessfully.',
-      data: AdministratorSerializer.new(resource).serializable_hash[:data][:attributes]
+      message: 'Logged in successfully.'
     }, status: :ok
   end
 
+  private
+
   def respond_to_on_destroy
-    if current_administrator
-      render json: {
-        message: "Logged out successfully"
-      }, status: :ok
-    else
-      render json: {
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
-    end
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    yield if block_given?
+
+    render json: {
+      message: "Logged out successfully."
+    }, status: :ok
   end
 end
