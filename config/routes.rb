ff2384d8e1fe0d109836270e933d729f0b343fa5
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-07-02 16:42:16 -0300

    refact: devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   get 'search/vehicle_by_plate'
-  devise_for :administrator, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'sign_up'
-  },
-  controllers: {
-    sessions: 'administrators/sessions',
-    registrations: 'administrators/registrations'
-  }
+  devise_for :administrators, skip: [:sessions, :registrations, :passwords]
+  devise_scope :administrator do
+    # Sessions
+    post   '/login',  to: 'administrators/sessions#create',  as: :administrator_session
+    delete '/logout', to: 'administrators/sessions#destroy', as: :destroy_administrator_session
+    # Registrations
+    put    '/sign_up',  to: 'administrators/registrations#update',  as: nil
+    delete '/sign_up',  to: 'administrators/registrations#destroy', as: nil
+    post   '/sign_up',  to: 'administrators/registrations#create',  as: nil
+    patch  '/sign_up',  to: 'administrators/registrations#update',  as: :administrator_registration
+  end
 
   get '/current_administrator', to: 'current_administrator#index'
   resources :movements, except: [:index, :show]
