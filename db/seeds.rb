24f6cb4ad3792f426f26e56146f71d7a7c421aa8
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-07-03 12:03:36 -0300

    update: seeds database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,61 +1,85 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'colorize'
+require 'faker'
 
-name = 'Admin'
-email = 'admin@sei.co'
-password = 'admin123'
-
-administrator = Administrator.find_by(email: email)
-
-n_vacancies = 30 # Número desejado de registros
-
-# Creating one administrator
-
-unless administrator
+def create_administrators
+  email = 'admin@sei.co'
+  password = 'admin123'
+   
   administrator = Administrator.create!(
-    name: name,
+    name: Faker::Name.first_name,
     email: email,
-    password: password,
-    password_confirmation: password
+    password: password
   )
-
-  puts "#1 - Usuário ##{administrator.id} criado com sucesso!"
+  
+  puts "#1 - Administrador #{administrator.name} criado com sucesso!".green
 end
 
-# Creating one parking
+def create_parkings
+  min_cost_per_hour = 0.0
+  max_cost_per_hour = 6.0
 
-min_cost_per_hour = 0.0
-max_cost_per_hour = 6.0
+  administrator = Administrator.last
 
-parking = Parking.create!(
-    name: Faker::Company.name,
-    address: Faker::Address.full_address,
-    opening_time: Time.zone.parse('06:00'),
-    closing_time: Time.zone.parse('18:00'),
-    cost_per_hour: rand(min_cost_per_hour..max_cost_per_hour),
-    administrator_id: administrator.id
-)
+  if administrator
+    puts "#2 - Usando o último administrador cadastrado.".yellow
 
-puts "#2 - Estacionamento criado com sucesso!"
+    Parking.create!(
+      name: Faker::Company.name,
+      address: Faker::Address.full_address,
+      opening_time: Time.zone.parse('06:00'),
+      closing_time: Time.zone.parse('18:00'),
+      cost_per_hour: rand(min_cost_per_hour..max_cost_per_hour),
+      administrator_id: administrator.id
+    )
 
-# Creating n vacancies
+    puts "#2 - Estacionamento criado com sucesso!".green
+    return
+  end
 
-kinds = Vacancy.kinds.keys # Array com os valores possíveis para o enum kind
-
-n_vacancies.times do
-  code = Faker::Alphanumeric.unique.alphanumeric(number: 6) # Gera um código alfanumérico único com 6 caracteres
-  kind = kinds.sample # Seleciona um valor aleatório do enum kind
-
-  Vacancy.create!(
-    code: code, 
-    kind: kind,
-    parking_id: parking.id
-  )
+  puts "#2 - Não foi possível criar estacionamento, pois não foi encontrado um administrador.".red
 end
 
-puts "#3 - Vagas criadas com sucesso!"
+def create_vacancies
+  n_vacancies = 18
+  
+  kinds = Vacancy.kinds.keys
+  
+  parking = Parking.last
+
+  if parking
+    puts "#3 - Usando o último estacionamento cadastrado.".yellow
+    
+    n_vacancies.times do
+      code = Faker::Alphanumeric.unique.alphanumeric(number: 6)
+      kind = kinds.sample
+
+      Vacancy.create!(
+        code: code, 
+        kind: kind,
+        parking_id: parking.id
+      )
+    end
+  
+    puts "#3 - Vagas criadas com sucesso!".green
+    return
+  end
+
+  puts "#3 - Não foi possível criar vagas, pois não foi encontrado um estacionamento.".red
+end
+
+def run_all
+  create_administrators
+  create_parkings
+  create_vacancies
+end
+
+case ENV['SEEDS_SECTION']
+when 'administrators'
+  create_administrators
+when 'parkings'
+  create_parkings
+when 'vacancies'
+  create_vacancies
+else
+  run_all
+end
